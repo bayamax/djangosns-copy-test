406d704cf495735a2cc2d79333e6b3a16b65bc59
YasunariIguchi <iguchi.yasunari.0219@gmail.com>
2022-02-11 02:26:16 +0900

    cafeページを表示(文字のみ)

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('post.urls')),
     path('accounts/', include('accounts.urls')),
+    path('cafe/', include('cafe.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
