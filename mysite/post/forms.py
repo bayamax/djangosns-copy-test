ae25dfbbb29edb25d68dca5a0c9bef19cfdb8ccc
bayamax <96761258+bayamax@users.noreply.github.com>
2022-03-14 15:06:09 +0900

    会員登録機能

--- mysite/post/forms.py
+++ mysite/post/forms.py
@@ -1,5 +1,15 @@
 from django import forms
 from .models import Post
+#from django.contrib.auth.models import User
+from accounts.models import  User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignUpForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
 
 class PostCreateForm(forms.ModelForm):
--- mysite/post/views.py
+++ mysite/post/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, redirect
 from .forms import PostCreateForm
 from .models import Post
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import SignUpForm
+
+class SignUp(CreateView):
+    form_class = SignUpForm
+    template_name = "accounts/signup.html" 
+    success_url = reverse_lazy('post:post_list')
+
+    def form_valid(self, form):
+        user = form.save() # formの情報を保存
+        login(self.request, user) # 認証
+        self.object = user 
+        return HttpResponseRedirect(self.get_success_url()) # リダイレクト
 
 # Create your views here.
 
