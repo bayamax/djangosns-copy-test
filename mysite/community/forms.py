d7e0549c8362eca6b99e0b3bb933590f5b8934b3
YasunariIguchi <iguchi.yasunari.0219@gmail.com>
2022-04-03 16:10:18 +0900

    formsとmodelsを作成

---
+++ mysite/community/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import Community
+
+class CommunityCreateForm(forms.ModelForm):
+    class Meta:
+        model = Community
+        fields = (
+            'name',
+        )
--- mysite/community/models.py
+++ mysite/community/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class Community(models.Model):
+    name = models.TextField()
+    created_by = models.ForeignKey(get_user_model(), default='', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
