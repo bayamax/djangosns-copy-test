36597a44ab5ddcd1a7b1aac68281df94bbfd76d7
YasunariIguchi <iguchi.yasunari.0219@gmail.com>
2022-04-03 15:13:06 +0900

    アカウントページでそのアカウントのコメントを表示

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from post.models import Post
 
 
 def user_profile(request, username):
+    user_model = get_user_model().objects.get(username=username)
     context = {
-        'User': get_user_model().objects.get(username=username),
+        'User': user_model,
+        'post_list': Post.objects.filter(author=user_model.id),
     }
     return render(request, 'accounts/user_profile.html', context)
