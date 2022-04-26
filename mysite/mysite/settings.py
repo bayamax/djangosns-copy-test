a1ed08be80bb2f9d65a0eb727bc9a7e42f7bac80
bayamax <96761258+bayamax@users.noreply.github.com>
2022-04-27 03:05:21 +0900

    dj_database_url

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -74,9 +74,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
