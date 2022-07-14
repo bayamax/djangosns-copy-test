32604d8ec427efd60c725eff36ae4f294af304a5
bayamax <bayamax@icloud.com>
2022-07-14 23:10:54 +0900

    admin追加

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 from django.contrib.auth import get_user_model
 
-from .models import Connection
+from .models import Connection,Mycommunity
 
 admin.site.register(get_user_model())
 
 admin.site.register(Connection)
+
+admin.site.register(Mycommunity)
